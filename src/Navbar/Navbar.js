e0c1cc5670571284092ec4fb69cdd44a46a28e91
vladimir290702 <vladimirovichh@abv.bg>
2021-11-05 18:26:32 +0200

    create Navbar component

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <header>
+            <a className="title" href="#home">METFLIX</a>
+
+            <nav class="navbar">
+                <ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#register">Register</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
