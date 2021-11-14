a51f58275f951c4bbdf15c636b7b9dce260b4158
vladimir290702 <vladimirovichh@abv.bg>
2021-11-14 19:56:20 +0200

    add TMDB endpoints

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Footer from "./components/Footer/Footer";
 import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <Navbar />
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
         <footer className="site-footer">
             <div className="container">
                 <div className="social-media">
-                    <a href="/facebook" className="fa fa-facebook"></a>
-                    <a href="/twitter" className="fa fa-twitter"></a>
-                    <a href="/instagram" className="fa fa-instagram"></a>
-                    <a href="/youtube" className="fa fa-youtube"></a>
+                    <a href="#" className="fa fa-facebook"></a>
+                    <a href="#" className="fa fa-twitter"></a>
+                    <a href="#" className="fa fa-instagram"></a>
+                    <a href="#" className="fa fa-youtube"></a>
                 </div>
             </div>
             <div className="container">
